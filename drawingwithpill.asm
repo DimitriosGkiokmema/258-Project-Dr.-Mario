09fdb4a135d61bb80e3e6e32d70fa665c42cc71c
erisdefne <80827332+erisdefne@users.noreply.github.com>
2025-03-31 15:05:26 -0400

    Update drawingwithpill.asm
    Added names and student numbers

--- drawingwithpill.asm
+++ drawingwithpill.asm
@@ -1,8 +1,8 @@
 ################# CSC258 Assembly Final Project: Dr. Mario ###################
 # This file contains our implementation of Dr Mario.
 #
-# Student 1: Name, Student Number
-# Student 2: Name, Student Number (if applicable)
+# Student 1: Defne Eris, 1010127596
+# Student 2: Dimitrios Gkiokmema, 010372286
 #
 # We assert that the code submitted here is entirely our own 
 # creation, and will indicate otherwise when it is not.
